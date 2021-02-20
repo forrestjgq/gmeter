2fa8a0970e0ee2a8b31d7b66f33eec9f432dea4a
江国庆 <mr.jianggq@163.com>
2021-02-20 11:06:49 +0800

    add config list test

--- internal/meter/exec_test.go
+++ internal/meter/exec_test.go
@@ -25,6 +25,26 @@ func TestExecute(t *testing.T) {
 		t.Fatalf(err.Error())
 	}
 }
+func TestExecuteList(t *testing.T) {
+
+	opt := &config.GOptions{
+		Template: "test/base/base.json",
+		Configs: []string{
+			"test/base/ping.json",
+			"test/client",
+			"test/client/all.list",
+		},
+		HTTPServerCfg:  "test/server/server.json",
+		ArceeServerCfg: "",
+		Call:           "",
+		Final:          "test/base/ping.json",
+	}
+
+	err := Execute(opt)
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+}
 func TestExecuteImports(t *testing.T) {
 
 	opt := &config.GOptions{
